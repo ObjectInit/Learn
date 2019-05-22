0fbaa828fd1fc0daa02e0e40020062afeb3136db
coden.liu <coden.liu@ShiJiWorkstation>
2019-05-22 13:08:59 +0800

    添加单例模式

---
+++ Learn/DesignPattern/SimpleFactory.cs
@@ -0,0 +1,22 @@
+﻿#region  <<版本注释>>
+/* ========================================================== 
+// <copyright file="SimpleFactory.cs" company="Shiji.BO.CS">
+// Copyright (c) Shiji.BO.CS. All rights reserved.
+// </copyright>
+* 功能描述：SimpleFactory 
+* 创 建 者：Administrator 
+* 创建时间：2019/5/22 9:29:19 
+* =============================================================*/
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn.DesignPattern
+{
+    public class SimpleFactory
+    {
+    }
+}
